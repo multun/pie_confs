02f58d08261af2ae8e0b1813ac27ae2e3b199881
Victor "multun" Collod <victor.collod@epita.fr>
2017-09-10 13:10:35 +0200

    update bashit install script

--- install_bashit.sh
+++ install_bashit.sh
@@ -5,4 +5,7 @@ git clone https://github.com/Bash-it/bash-it
 cat << EOF >> ~/afs/.confs/bashrc
 export BASH_IT=~/afs/tools/bash-it/
 export BASH_IT_THEME=minimal
+. ~/afs/tools/bash-it/bash_it.sh
 EOF
+
+bash  -c '. ~/afs/tools/bash-it/bash_it.sh && bash-it enable plugin base'
