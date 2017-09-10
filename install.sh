84f7908d632d265fcf3001e244e7436d016be8f7
Victor "multun" Collod <victor.collod@epita.fr>
2017-09-10 13:11:11 +0200

    install.sh: symlink empty dirs

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-e_dir_list="mozilla ssh"
-dot_list="bashrc config emacs gitconfig gitignore mozilla msmtprc muttrc slrnrc ssh Xdefaults Xresources xfce4"
+e_dir_list="mozilla ssh thunderbird"
+dot_list="local bashrc config emacs gitconfig gitignore msmtprc muttrc slrnrc Xdefaults Xresources xfce4"
 
 for d in $e_dir_list; do
   mkdir -p "$AFS_DIR/.confs/$d"
 done
 
-for f in $dot_list; do
+for f in $e_dir_list $dot_list; do
   rm -rf "$HOME/.$f"
   ln -s "$AFS_DIR/.confs/$f" "$HOME/.$f"
 done
+
+"$SHELL" ~/afs/.confs/dotfiles/install.sh
